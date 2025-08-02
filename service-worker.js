8b26c5e7f8402359a0dce44abe8fb5a68ee45af4
SEtokoe <su.kyikyu@gmail.com>
2025-08-02 12:40:48 +0700

    Initial Commit

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'survey-app-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192x192.png',
+  'icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
